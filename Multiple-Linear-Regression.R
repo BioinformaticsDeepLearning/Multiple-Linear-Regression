ce46a49176e862ae947c1aa845a52efd417cd8d4
Alisha Parveen <49845542+aparveen-prog@users.noreply.github.com>
2020-07-22 22:10:12 +0200

    Update Multiple-Linear-Regression.R

--- Multiple-Linear-Regression.R
+++ Multiple-Linear-Regression.R
@@ -1,13 +1,13 @@
 #MULTIPLE LINEAR REGRESSION#
-It is the extension of simple linear regression with multiple variable with given equations:
-  y= a + b1x1 + b2x2 +...bnxn
- Where,
-y is the response variable.
-a (intercept), b1, b2...bn are the coefficients.
-x1, x2, ...xn are the predictor variables.
+# It is the extension of simple linear regression with multiple variable with given equations:
+#  y= a + b1x1 + b2x2 +...bnxn
+# Where,
+# y is the response variable.
+# a (intercept), b1, b2...bn are the coefficients.
+# x1, x2, ...xn are the predictor variables.
 
 #Input dataset for construction of the regression model#
-  input <- mtcars[,c("mpg","disp","hp","wt")]
+  input <- mtcars[,c("mpg","disp","hp","wt")]   #Dataset present in R#
   print(head(input))
 
 #Create a relationship between the variables#
